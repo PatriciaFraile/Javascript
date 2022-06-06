acc79aa6de16342ca34c1afa28cbe5147560944e
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2022-06-06 10:03:37 +0200

    subir

---
+++ 01_inicio/utils/js/variables.js
@@ -0,0 +1,8 @@
+console.log('Variables cargado');
+//let --> ámbito global y de clase
+//var --> ámbito global --> no se utiliza
+//const
+let nombre = "Borja";
+let edad = 37;
+let fecha =  new Date ();
+
