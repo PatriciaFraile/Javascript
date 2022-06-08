a8dba5c374c1c54de4a1b3126314801139888f87
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2022-06-08 10:17:33 +0200

    subir

---
+++ 01_inicio/utils/js/arrays.js
@@ -0,0 +1,35 @@
+let elementos =[1,2,4,5,6,7]; //number
+let equipos = ["madrid","barsa","sevilla","atletico","getafe"]; //string
+let cosas = [1,2,3,4,"madrid", false]; //any
+/*
+//acceder posicion
+console.log(equipos[0]);
+equipos[0] = "leganes";
+console.log(equipos);
+console.log(equipos.length);
+//agregar en la ultima posicion
+equipos[equipos.length] = "cadiz";
+console.log(equipos);
+//metodo push se añade al final del arraylist
+console.log(equipos.push("leganes","alcorcon","cadiz"));
+console.log(equipos);
+//metodo pop para quitar
+console.log(equipos.pop());
+console.log(equipos);
+//unshift insertar un nuevo elemento al principio 
+console.log(equipos.unshift("leganes","alcorcon","cadiz"));
+console.log(equipos);
+//shift quita el primer elemento
+console.log(equipos.shift());
+console.log(equipos);
+*/
+//filter ---> filtrar por x posicion
+
+
+
+
+
+
+
+
+
--- 01_inicio/utils/js/cadenas.js
+++ 01_inicio/utils/js/cadenas.js
@@ -1,12 +1,75 @@
-let frase = "Esto es un ejemplo de String";
-console.log(frase.charAt(0).toLowerCase());
-let consonantes;
-let vocales = ["a","e","i","o","u"]
-let contador;
+/* let frase = "Esto es un ejemplo de string";
+frase = frase.replaceAll(" ", "");
+console.log(frase);
+let vocales = 0,
+  consonantes = 0;
 for (let index = 0; index < frase.length; index++) {
-    if(frase.charAt(index)==vocales){
-        console.log(index);
-        
-    }  
-
+  if (
+    frase.charAt(index).toLowerCase() == "a" ||
+    frase.charAt(index).toLowerCase() == "e" ||
+    frase.charAt(index).toLowerCase() == "i" ||
+    frase.charAt(index).toLowerCase() == "o" ||
+    frase.charAt(index).toLowerCase() == "u"
+  ) {
+    vocales++;
+  } else {
+    consonantes++;
+  }
 }
+alert(
+  `El numero de vocales es ${vocales} y el de consonantes es ${consonantes}`
+); */
+/*let frase = "Esto es un ejemplo de frase para analizar";
+let estaLetra = frase.includes("E");
+let subfrase = frase.slice(0,5);*/
+let frase = prompt("Escribe un correo");
+let numeroArrobas = 0
+let posicionArroba = 0;
+for (let index = 0; index < frase.length; index++) {
+    if(frase.charAt(index)=="@"){
+        numeroArrobas++;
+        posicionArroba = index;
+    }    
+}
+console.log(numeroArrobas);
+
+if(numeroArrobas==1){
+    let dominio = frase.slice(posicionArroba + 1,frase.length);
+    console.log(dominio);
+}else{
+    alert(`No valido`);
+    
+}
+console.log("Con el for");
+
+let numeroPuntos = 0;
+let posicionPuntos = 0;
+for (let index = 0; index < frase.length; index++) {
+    if(frase.charAt(index)== "."){
+        numeroPuntos++;
+        posicionPuntos = index;
+    }
+}
+console.log(numeroPuntos);
+
+if (numeroPuntos==1){
+    let dominio = frase.slice(posicionPuntos + 1 , frase.length);
+    console.log(dominio);
+}else{
+    alert(`No valido`)
+}
+
+console.log("Con el split");
+let partesCorreo = frase.split("@");
+if(partesCorreo.length==2){
+    let dominio = partesCorreo[1].split('.');
+    if(dominio.length==2 && (dominio[1]=="es"|| dominio[1]=="com")){
+        console.log("correo valido");
+    }else{
+        console.log("correo invalido");
+        
+    }
+}else{
+    console.log("correo invalido");
+}
+
