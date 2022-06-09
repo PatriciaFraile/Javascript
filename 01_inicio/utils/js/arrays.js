2be59031ab7a58dbc717817caa9d8a0bce471481
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2022-06-09 09:27:44 +0200

    subir

--- 01_inicio/utils/js/arrays.js
+++ 01_inicio/utils/js/arrays.js
@@ -33,26 +33,36 @@ console.log(equipos);
 let numeros = [1,2,3,4,5,6,7,8,9,10];
 //mostrar los elementos de un array
 let i = 0;
-/*while(i<numeros.length){
+while(i<numeros.length){
     console.log(numeros[i]);
     i++;
 }
-*/
-/*for (let index = 0; index < numeros.length; index++) {
+for (let index = 0; index < numeros.length; index++) {
     console.log(numeros[index]);    
 }
-*/
-/*numeros.forEach(element => {
+numeros.forEach(element => {
     console.log(element);  
 });
-*/
-/*while(i<numeros.length){
+
+while(i<numeros.length){
     console.log(numeros[i]+1);
     i++;
 }
-*/
+let suma = 0
 
+numeros.forEach(element => {
+    suma+=element 
+});
+console.log("la media es" + suma/numeros.length);
 
+let arrayAleatorio=[];
+for (let index = 0; index < 20; index++) {
+    arrayAleatorio[index]= Math.random();    
+}
+arrayAleatorio.forEach(element => {
+    console.log(element);
+    
+});
 
 
 
---
+++ 01_inicio/utils/js/funciones.js
@@ -0,0 +1,22 @@
+// funciones declaradas
+//funciones anonimas - funciones de flecha 
+
+//function nombre (parametros , parametros) {
+    //cuerpo
+    //return
+//}
+
+//nombre(1,2)
+
+//let funcionAnonima = function (parametros){}
+//funcionAnonima(parametros)
+
+//let flecha=(param1 , param2)=> { return cuerpo}
+//flecha(param1,param2)
+
+function sumaNumeros(operadorUno , operadorDos){
+    let suma = operadorUno + operadorDos;
+    console.log(suma);
+    
+}
+sumaNumeros(5,10);
