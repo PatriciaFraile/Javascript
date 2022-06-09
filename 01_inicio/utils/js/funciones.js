f4e43d7b1afff41189dc10622f2812a4615e819e
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2022-06-09 11:20:36 +0200

    subir

--- 01_inicio/utils/js/funciones.js
+++ 01_inicio/utils/js/funciones.js
@@ -13,10 +13,55 @@
 
 //let flecha=(param1 , param2)=> { return cuerpo}
 //flecha(param1,param2)
-
+/*
 function sumaNumeros(operadorUno , operadorDos){
     let suma = operadorUno + operadorDos;
     console.log(suma);
     
 }
-sumaNumeros(5,10);
+//sumaNumeros(5,10);
+sumaNumeros(10,60,67);
+
+function sumaDefecto(operandoUno , operandoDos = 0) {
+    let suma = operandoUno + operandoDos;
+    console.log(suma);
+}
+sumaDefecto(1,3);
+sumaDefecto(7);
+
+function functionSinParametros(){
+    console.log("funcion llamada");
+    console.log(arguments);   
+}
+functionSinParametros(1,2,3,4,5,6,7,8);
+
+function functionRetorno(op1,op2){
+    let suma = op1 + op2;
+    return suma;
+}
+*/
+/*
+let num1 = Number (prompt('Escribe un numero'));
+let num2 = Number(prompt('Escribe un numero'));
+function operacion (num1 , num2){
+    alert(`Suma: ${num1+num2}
+    Resta ${num1-num2}
+    Multiplicacion ${num1*num2}
+    Division ${num1/num2}`)
+}
+if(!NaN(num1) || !NaN(num2)){
+    operacion(num1,num2);
+}
+let frase = promt('Introduce una frase');
+let letra = promt('Introduce una letra');
+let contador = 0;
+if(frase.includes(letra)){
+    frase.split('').forEach(element => {
+        if(element==letra){
+            contador++;
+        }
+    });
+    alert(`La letra ${letra} esta incluida y aparece ${contador} veces`)
+}
+*/
+
