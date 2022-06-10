252df5fe3697aed41805ac8823efe69808b1157c
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2022-06-10 10:10:56 +0200

    subir

--- 02_dinamismo/utils/js/dinamismo.js
+++ 02_dinamismo/utils/js/dinamismo.js
@@ -3,28 +3,28 @@
 
 //buscar elementos : 
 //tag
-//let listas = document.getElementsByTagName("ul");
+let listas = document.getElementsByTagName("ul");
 let elementosLista = document.getElementsByTagName("li");
-/*console.log(listas);
+console.log(listas);
 elementosLista[0].innerText = "Modificacion de la propiedad"
 //innerText el contenido del li
 console.log(elementosLista[0].innerText);
 for (let index = 0; index < elementosLista.length; index++) {
     console.log(elementosLista[index].innerText);
     index++;   
-}*/
+}
 let listar = ["Matemáticas","Historia", "Fisica","Biología","Lengua"];
 
-/*setTimeout(() => {
+setTimeout(() => {
     for (let index = 0; index < listar.length; index++) {
         elementosLista[index].innerText= listar[index];
     }
-}, 5000);*/
-/*setInterval(() => {
+}, 5000);
+setInterval(() => {
     console.log("Ejecución repetiva");
     
 }, 2000);
-*/
+
 //class
 let asignaturasSegundo = document.getElementsByClassName("list-group-item");
 console.log(asignaturasSegundo);
