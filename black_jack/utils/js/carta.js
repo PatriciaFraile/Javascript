98f0ba26b889551e692b89c704a631afb164f241
PatriciaFraile <patricia.fraile@cesjuanpablosegundo.es>
2022-06-15 18:18:41 +0200

    subir

--- black_jack/utils/js/carta.js
+++ black_jack/utils/js/carta.js
@@ -1,17 +1,17 @@
 class carta {
-    valor;
-    ruta;
-  
-    constructor(ruta, valor) {
-      this.valor = valor;
-      this.ruta = `./utils/images/${ruta}.png`;
-    }
-  
-    get getValor() {
-      return this.valor;
-    }
-  
-    get getRuta() {
-      return this.ruta;
-    }
+  valor;
+  ruta;
+
+  constructor(ruta, valor) {
+    this.valor = valor;
+    this.ruta = `./utils/images/${ruta}.png`;
   }
+
+  get getValor() {
+    return this.valor;
+  }
+
+  get getRuta() {
+    return this.ruta;
+  }
+}
